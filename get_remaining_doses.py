a5b247685514a3ea503fd299472c791e490b67bc
Sahil Modi <sm34524@gmail.com>
2020-12-15 12:50:13 -0600

    added location param for remaining doses

--- get_remaining_doses.py
+++ get_remaining_doses.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from lib.medicine_database import MedicineDatabase
 
 ap = argparse.ArgumentParser()
-ap.add_argument("--dose", "-d", required=True, type=int, help="Units per kilogram required")
+ap.add_argument("--dose", "-d", required=True, type=int, help="Units per kilogram required.")
+ap.add_argument("--location", "-l", default="school", type=str, help="Location of the doses")
 args = ap.parse_args()
 
 if __name__ == "__main__":
@@ -12,6 +13,8 @@ if __name__ == "__main__":
     medicines = database.get_available()
     total_units_left = 0
     for hum in medicines:
+        if hum.location != args.location:
+            continue
         total_units_left += hum.units
     print("Total Units Remaining:", total_units_left)
     dosage = int(total_units_left//(args.dose*database.weight_kg))
